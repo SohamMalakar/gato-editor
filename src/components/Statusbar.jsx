9f6fbd1adbb7d0d1144f30d89f5bb7e72b696bf6
Soham Malakar <soham.pc4756@gmail.com>
2025-09-21 12:30:56 +0530

    cleanup unnecessary code

--- src/components/Statusbar.jsx
+++ src/components/Statusbar.jsx
@@ -1,6 +1,6 @@
 function Statusbar() {
   return (
-    <div className="flex justify-end items-center h-8 w-full px-3 text-sm font-medium text-neutral-100 select-none overflow-hidden">
+    <div className="flex justify-end items-center h-8 w-full px-3 text-sm font-medium text-neutral-100 select-none">
       <div>Statusbar</div>
     </div>
   )
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,5 +1,3 @@
-
-import * as React from 'react'
 import { FaLinux, FaPlaystation, FaXbox } from "react-icons/fa";
 
 function Toolbar({ handleClick }) {
